c0499ce3cb67c56d36b833176e3789cdb04d33a6
Ian Shi <ianshi1026@gmail.com>
2025-12-07 18:06:04 -0500

    Turret controller exposes services for setting flywheel and shooting

--- src/turret_controller/include/turret_controller/turret_controller.hpp
+++ src/turret_controller/include/turret_controller/turret_controller.hpp
@@ -3,6 +3,9 @@
 #include "pluginlib/class_list_macros.hpp"
 #include "controller_interface/controller_interface.hpp"
 #include "std_msgs/msg/float64.hpp"
+#include "std_srvs/srv/set_bool.hpp"
+#include "std_srvs/srv/trigger.hpp"
+#include <atomic>
 
 namespace turret_controller {
     class TurretController : public controller_interface::ControllerInterface {
@@ -19,13 +22,20 @@ namespace turret_controller {
             controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
         private:
-            double pan_angle_;
-            double tilt_angle_;
-            double trigger_distance_;
-            bool flywheel_enabled_;
+            double pan_angle_{0.0};
+            double tilt_angle_{0.0};
+            bool flywheel_enabled_{false};
+            std::atomic<double> trigger_distance_{0.0};
+            std::atomic<bool> trigger_requested_{false};
 
             rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr pan_angle_sub_;
             rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr tilt_angle_sub_;
-            // TODO: Add services for trigger and flywheel control
+            rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr trigger_service_;
+            rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr flywheel_service_;
+
+            /**
+             * @brief Extends the trigger, waits a short time, then retracts the trigger.
+             */
+            void trigger_shoot();
     };
 }
--- src/turret_controller/src/turret_controller.cpp
+++ src/turret_controller/src/turret_controller.cpp
@@ -24,11 +24,6 @@ namespace turret_controller {
     }
 
     controller_interface::CallbackReturn TurretController::on_configure([[maybe_unused]] const rclcpp_lifecycle::State & previous_state) {
-        pan_angle_ = 0.0;
-        tilt_angle_ = 0.0;
-        trigger_distance_ = 0.0;
-        flywheel_enabled_ = false;
-
         return controller_interface::CallbackReturn::SUCCESS;
     }
 
@@ -49,12 +44,38 @@ namespace turret_controller {
             }
         );
 
+        trigger_service_ = get_node()->create_service<std_srvs::srv::Trigger>(
+            "/turret_controller/trigger",
+            [this](const std_srvs::srv::Trigger::Request::SharedPtr request, std_srvs::srv::Trigger::Response::SharedPtr response) {
+                (void)request;
+                if (trigger_requested_.load()) {
+                    response->success = false;
+                    response->message = "Turret is already firing.";
+                    return;
+                }
+
+                trigger_requested_.store(true);
+                std::thread(&TurretController::trigger_shoot, this).detach();
+                response->success = true;
+            }
+        );
+
+        flywheel_service_ = get_node()->create_service<std_srvs::srv::SetBool>(
+            "/turret_controller/set_flywheel",
+            [this](const std_srvs::srv::SetBool::Request::SharedPtr request, std_srvs::srv::SetBool::Response::SharedPtr response) {
+                flywheel_enabled_ = request->data;
+                response->success = true;
+            }
+        );
+
         return controller_interface::CallbackReturn::SUCCESS;
     }
 
     controller_interface::CallbackReturn TurretController::on_deactivate([[maybe_unused]] const rclcpp_lifecycle::State & previous_state) {
         pan_angle_sub_.reset();
         tilt_angle_sub_.reset();
+        trigger_service_.reset();
+        flywheel_service_.reset();
         return controller_interface::CallbackReturn::SUCCESS;
     }
 
@@ -65,13 +86,20 @@ namespace turret_controller {
             } else if (command_interface.get_name() == "tilt_joint/angle") {
                 (void)command_interface.set_value(tilt_angle_);
             } else if (command_interface.get_name() == "trigger_joint/distance") {
-                (void)command_interface.set_value(trigger_distance_);
+                (void)command_interface.set_value(trigger_distance_.load());
             } else if (command_interface.get_name() == "flywheel/enabled") {
                 (void)command_interface.set_value(flywheel_enabled_ ? 1.0 : 0.0);
             }
         }
 
         return controller_interface::return_type::OK;
+    }
+
+    void TurretController::trigger_shoot() {
+        trigger_distance_.store(1.0); // Extend the trigger
+        rclcpp::sleep_for(std::chrono::milliseconds(1000));
+        trigger_distance_.store(0.0); // Retract the trigger
+        trigger_requested_.store(false);
     }
 }
 
