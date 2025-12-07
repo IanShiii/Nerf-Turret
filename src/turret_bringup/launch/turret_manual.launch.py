e63cf88ba132c266db274817a3be68962742bdb2
Ian Shi <ianshi1026@gmail.com>
2025-12-07 02:22:03 -0500

    toss all ros2 control related things into its own launch file

---
+++ src/turret_bringup/launch/turret_manual.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import FrontendLaunchDescriptionSource, PythonLaunchDescriptionSource
+import os
+
+def generate_launch_description():
+    ros2_control_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('turret_bringup'),
+                'launch',
+                'ros2_control.launch.py'
+            )
+        )
+    )
+
+    return LaunchDescription([
+        ros2_control_launch
+    ])
--- src/turret_bringup/setup.py
+++ src/turret_bringup/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'turret_bringup'
 
@@ -10,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/turret_bringup.launch.py']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
